f7dae35098eb9e6642b84cfffe32184aba691e67
mohammedfuzaila <anaikarmohammedfuzail57@gmail.com>
2026-05-31 16:24:54 +0530

    fix 4

--- portfolio_backend/wsgi.py
+++ portfolio_backend/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings')
 
+# Automatically run migrations on startup to fix Render missing table errors
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception:
+    pass
+
 application = get_wsgi_application()
